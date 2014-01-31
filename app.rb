fe86da05d9315c264dd578e2056eb8bf55783207
home <charly.palencia@koombea.com>
2014-01-31 08:28:15 -0500

    add show place

--- app.rb
+++ app.rb
@@ -3,17 +3,24 @@ require 'bundler'
 
 Bundler.require
 Dotenv.load
-
-get '/' do
   client = Foursquare2::Client.new(
     client_id: ENV["API_CLIENT"],
     client_secret: ENV["CLIENT_SECRET"],
-     api_version: "20130815"
+     api_version: "20140130",
+     isFuzzed: true
   )
-  result = client.search_venues(
+
+get '/' do
+ result = client.search_venues(
     ll: "10.964112,-74.802582",
     near: "barranquilla",
     categoryId:"4bf58dd8d48988d1e2941735,4bf58dd8d48988d161941735"
   ).find { |data| data.first == "venues"}[1]
   haml :index, locals: {venues: result}
+end
+
+
+get "/place/:id" do
+ venue = client.venue(params[:id])
+ haml :show, locals: {venue: venue}
 end
